d6159da480060c4465255384036e2b3aac3306b6
Brandon Enright <bmenrigh@brandonenright.net>
2024-02-13 03:22:24 +0000

    Damage experimentation

---
+++ dmg_test.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+union du {
+    double d;
+    uint64_t u;
+};
+
+
+double adjacent_double(double d, long n) {
+    union du du = {.d = d};
+
+    /* Adjust +/- n steps */
+    du.u += n;
+
+    return du.d;
+}
+
+
+
+double damage(int pwr, int atk, int def, float acpm, float dcpm, double mult) {
+
+    /*double dmg = (0.5) * (double)pwr * (((double)atk * (double)acpm) / ((double)def * (double)dcpm)) * mult;*/
+
+    /* calculation in steps to try to maximize intermediate result */
+    double dmg;
+
+    /* increasing values */
+    dmg = (double)pwr;
+    dmg *= (double)atk;
+    dmg *= (double)mult;
+    dmg /= (double)dcpm;
+
+    /* decreasing values */
+    dmg *= (double)acpm;
+    dmg /= (double)def;
+    dmg /= (double)2;
+
+    return dmg + 1.0;
+}
+
+
+double damage2(int pwr, int atk, int def, float acpm, float dcpm, double mult) {
+
+    double dmg;
+
+    /* increasing values */
+    dmg = (double)pwr;
+    dmg *= (double)((float)atk * (float)acpm);
+    dmg *= (double)((float)mult);
+
+    /* decreasing values */
+    dmg /= (double)((float)def * (float)dcpm);
+    dmg /= (double)2;
+
+    return dmg + 1.0;
+}
+
+
+int main(void) {
+
+    float cpm_20 = 0.5974;
+    float cpm_34 = 0.7556855f;
+
+    double all_mults = 2.72999990613882824501930424578249810956501341386569286621;
+
+    fprintf(stderr, "\n== Calculating damage with multiplier %.16f ==\n", all_mults);
+
+    double d; /* damage */
+    d = damage(9, 185 + 15, 244 + 15, cpm_34, cpm_20, all_mults);
+    fprintf(stderr, "Mothim: %.16f (%d int)\n", d, (int)floor(d));
+
+    d = damage(4, 135 + 15, 244 + 15, cpm_34, cpm_20, all_mults);
+    fprintf(stderr, "Rat bite: %.16f (%d int)\n", d, (int)floor(d));
+
+    d = damage(5, 135 + 15, 244 + 15, cpm_34, cpm_20, all_mults);
+    fprintf(stderr, "Rat quick: %.16f (%d int)\n", d, (int)floor(d));
+
+    double d2_all_mults = (double)((float)1.3f * (float)1.2f * ((float)1.0f / (float)0.5714286f));
+    fprintf(stderr, "\n== Calculating damage2 with multiplier %.16f ==\n", d2_all_mults);
+
+    d = damage2(9, 185 + 15, 244 + 15, cpm_34, cpm_20, d2_all_mults);
+    fprintf(stderr, "Mothim: %.16f (%d int)\n", d, (int)floor(d));
+
+    d = damage2(4, 135 + 15, 244 + 15, cpm_34, cpm_20, d2_all_mults);
+    fprintf(stderr, "Rat bite: %.16f (%d int)\n", d, (int)floor(d));
+
+    d = damage2(5, 135 + 15, 244 + 15, cpm_34, cpm_20, d2_all_mults);
+    fprintf(stderr, "Rat quick: %.16f (%d int)\n", d, (int)floor(d));
+
+    return 0;
+}
+
+
+
